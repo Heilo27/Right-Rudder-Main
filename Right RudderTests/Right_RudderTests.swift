cfb2462d52a51960a413802aa4d6ebe5845b48ba
Ryan Primeau <cfdfirefighter@gmail.com>
2025-10-02 16:48:39 -0400

    Initial Commit

---
+++ Right RudderTests/Right_RudderTests.swift
@@ -0,0 +1,17 @@
+//
+//  Right_RudderTests.swift
+//  Right RudderTests
+//
+//  Created by Ryan on 10/2/25.
+//
+
+import Testing
+@testable import Right_Rudder
+
+struct Right_RudderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
